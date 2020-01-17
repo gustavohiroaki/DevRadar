ab48564df154ba769b45b2f573b62838987159b4
Gustavo Hiroaki <gustavo.hiroaki@csc.corp>
2020-01-17 08:12:34 -0300

    Code refactored and Added functionality

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,119 +1,44 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import api from "./services/api";
 import "./global.css";
 import "./App.css";
 import "./Sidebar.css";
 import "./Main.css";
 
+import DevItem from "./components/DevItem";
+import DevForm from "./components/DevForm";
+
 function App() {
+  const [devs, setDevs] = useState([]);
+
+  useEffect(() => {
+    async function loadDevs() {
+      const response = await api.get("/devs");
+
+      setDevs(response.data);
+    }
+
+    loadDevs();
+  }, []);
+
+  async function handleAddDev(data) {
+    const response = await api.post("/devs", data);
+
+    setDevs([...devs, response.data]);
+  }
+
   return (
     <div id="app">
       <aside>
         <strong>Cadastrar</strong>
-
-        <form>
-          <div className="input-block">
-            <label htmlFor="github_username">Usuário do Github</label>
-            <input name="github_username" id="github_username" />
-          </div>
-
-          <div className="input-block">
-            <label htmlFor="techs">Tecnologias</label>
-            <input name="techs" id="techs" />
-          </div>
-
-          <div className="input-group">
-            <div className="input-block">
-              <label htmlFor="latitude">Latitude</label>
-              <input name="latitude" id="latitude" />
-            </div>
-
-            <div className="input-block">
-              <label htmlFor="longitude">Longitude</label>
-              <input name="longitude" id="longitude" />
-            </div>
-          </div>
-
-          <button type="submit">Salvar</button>
-        </form>
+        <DevForm onSubmit={handleAddDev} />
       </aside>
 
       <main>
         <ul>
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars2.githubusercontent.com/u/47416709?s=460&v=4"
-                alt="Gustavo Hiroaki"
-              />
-              <div className="user-info">
-                <strong>Gustavo Hiroaki</strong>
-                <span>ReactJS, React Native, NodeJS</span>
-              </div>
-            </header>
-
-            <p>Isso é a biografia</p>
-
-            <a href="https://github.com/gustavohiroaki">
-              Acessar perfil do github
-            </a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars2.githubusercontent.com/u/47416709?s=460&v=4"
-                alt="Gustavo Hiroaki"
-              />
-              <div className="user-info">
-                <strong>Gustavo Hiroaki</strong>
-                <span>ReactJS, React Native, NodeJS</span>
-              </div>
-            </header>
-
-            <p>Isso é a biografia</p>
-
-            <a href="https://github.com/gustavohiroaki">
-              Acessar perfil do github
-            </a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars2.githubusercontent.com/u/47416709?s=460&v=4"
-                alt="Gustavo Hiroaki"
-              />
-              <div className="user-info">
-                <strong>Gustavo Hiroaki</strong>
-                <span>ReactJS, React Native, NodeJS</span>
-              </div>
-            </header>
-
-            <p>Biografia de teste</p>
-
-            <a href="https://github.com/gustavohiroaki">
-              Acessar perfil do github
-            </a>
-          </li>
-
-          <li className="dev-item">
-            <header>
-              <img
-                src="https://avatars2.githubusercontent.com/u/47416709?s=460&v=4"
-                alt="Gustavo Hiroaki"
-              />
-              <div className="user-info">
-                <strong>Gustavo Hiroaki</strong>
-                <span>ReactJS, React Native, NodeJS</span>
-              </div>
-            </header>
-
-            <p>Isso é a biografia</p>
-
-            <a href="https://github.com/gustavohiroaki">
-              Acessar perfil do github
-            </a>
-          </li>
+          {devs.map(dev => (
+            <DevItem key={dev._id} dev={dev} />
+          ))}
         </ul>
       </main>
     </div>
